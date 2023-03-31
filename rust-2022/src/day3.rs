db15a7c5d0111586b82fa500bdbb7e7b60c6d38f
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-03-31 11:31:54 -0700

    2022: Rust: Day 3 - part 2

--- rust-2022/src/day3.rs
+++ rust-2022/src/day3.rs
@@ -4,8 +4,8 @@ pub fn day3() {
     let day3_part1_solution = part1();
     println!("Day 3 - Part 1: {day3_part1_solution}");
 
-    // let day3_part2_solution = part2();
-    // println!("Day 3 - Part 2: {day3_part2_solution}");
+    let day3_part2_solution = part2();
+    println!("Day 3 - Part 2: {day3_part2_solution}");
 }
 
 const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -31,5 +31,26 @@ pub fn part1() -> u32 {
 
 pub fn part2() -> u32 {
     let input_text = utils::read_file("inputs/day3.txt");
-    todo!()
+
+    let mut grouped_sacs = Vec::new();
+    for (i, line) in input_text.lines().enumerate() {
+        if i % 3 == 0 {
+            grouped_sacs.push(Vec::new());
+        }
+        grouped_sacs.last_mut().unwrap().push(line);
+    }
+
+    let mut badge_letters = Vec::new();
+    for grouped_sac in grouped_sacs {
+        // unique between first 2
+        let badge = grouped_sac[0].chars().find(|c| {
+            grouped_sac[1].contains(c.to_owned()) && grouped_sac[2].contains(c.to_owned())
+        })
+        .unwrap();
+        badge_letters.push(badge);
+    }
+
+    badge_letters.into_iter()
+        .map(|letter| (ALPHABET.find(|l| l == letter).unwrap() + 1) as u32)
+        .sum()
 }
