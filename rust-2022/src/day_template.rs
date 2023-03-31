70c79d5f2452e75bb8c0f1f21d538895dc748d2b
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-03-30 21:29:13 -0700

    2022: Rust: Adding a template to copy from for each day

---
+++ rust-2022/src/day_template.rs
@@ -0,0 +1,20 @@
+use crate::utils;
+
+#[allow(dead_code)]
+pub fn dayN() {
+    let dayN_part1_solution = part1();
+    println!("Day N - Part 1: {dayN_part1_solution}");
+
+    let dayN_part2_solution = part2();
+    println!("Day N - Part 2: {dayN_part2_solution}");
+}
+
+pub fn part1() -> u32 {
+    let input_text = utils::read_file("inputs/dayN.txt");
+    todo!()
+}
+
+pub fn part2() -> u32 {
+    let input_text = utils::read_file("inputs/dayN.txt");
+    todo!()
+}
