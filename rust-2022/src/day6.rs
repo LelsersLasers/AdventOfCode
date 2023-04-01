a25a70ecb2b4ebb93887d729087c9926297fc67a
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-04-01 15:01:05 -0700

    2022: Rust: Day 6 - Part 2

--- rust-2022/src/day6.rs
+++ rust-2022/src/day6.rs
@@ -4,8 +4,8 @@ pub fn day6() {
     let day6_part1_solution = part1();
     println!("Day 6 - Part 1: {day6_part1_solution}");
 
-    // let day6_part2_solution = part2();
-    // println!("day 6 - part 2: {day6_part2_solution}");
+    let day6_part2_solution = part2();
+    println!("day 6 - part 2: {day6_part2_solution}");
 }
 
 pub fn part1() -> usize {
@@ -32,5 +32,22 @@ pub fn part1() -> usize {
 
 pub fn part2() -> usize {
     let input_text = utils::read_file("inputs/day6.txt");
-    todo!()
+    let first_line: &str = input_text.lines().take(1).collect::<Vec<&str>>()[0];
+    let letters = first_line.chars().collect::<Vec<char>>();
+
+    for i in 0..letters.len() - 14 {
+        let mut ls = Vec::new();
+        let mut all_dif = true;
+        for j in 0..14 {
+            if ls.contains(&letters[i + j]) {
+                all_dif = false;
+            }
+            ls.push(letters[i + j]);
+        }
+        if all_dif {
+            return i + 14;
+        }
+    }
+
+    panic!("Message marker not found!")
 }
